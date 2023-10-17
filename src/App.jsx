0e3c929912c15792c85844676a7b32a6fbaad19d
unknown <yadavnishu1905@gmail.com>
2023-10-17 09:03:50 +0530

    setting up plain vite project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import { Routes } from 'react-router-dom';
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+        {/* < Route path ="/"  element ={<Home/>}></Route>*/}
+      </Routes>
+       
+    </>
+  )
+}
+
+export default App;
